e203460770a9bb2f257ff60af5be4e2e3cf3d190
Evgeniy <evg54@internet.ru>
2024-07-17 16:39:48 +0700

    4.1 CR-ops

---
+++ Module_2/4.1/main.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "header.h"
+
+void menu();
+
+int main() {
+    menu();
+    return 0;
+}
+
+void menu() {
+    Item* head = NULL;
+    while (1) {
+        printf("----------------------------------------\n");
+        printf("Выберите действие:\n1. Просмотреть список контактов\n2. Добавить новый контакт\n3. Удалить контакт\n4. Редактировать контакт\n0. Выход\n");
+        printf("----------------------------------------\n");
+        int c;
+        scanf("%d", &c);
+        switch (c) {
+            case 0:
+                return;
+            case 1:
+                print_list(head);
+                break;
+            case 2: {
+                system("clear");
+                head = add_contact(head);
+                break;
+            }
+            case 3: {
+                system("clear");
+                printf("Введите номер контакта: ");
+                int n;
+                scanf("%d", &n);
+                head = del_contact(head, n - 1);
+                break;
+            }
+            case 4: {
+                system("clear");
+                printf("Введите номер контакта: ");
+                int n;
+                scanf("%d", &n);
+                // if (edit_contact(head, n) != 0) {
+                //     system("clear");
+                //     printf("Ошибка редактирования!\n");
+                // }
+                break;
+            }
+            default: {
+                system("clear");
+                printf("Введено неверное значение!\n");
+                break;
+            }
+        }
+    }
+}
